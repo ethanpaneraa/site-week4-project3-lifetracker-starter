921cc7c5e5838525aa973e44b71a647f9cc84500
Ethan Pineda <joshuapineda66@gmail.com>
2023-06-28 23:15:02 -0700

    sql schema done

---
+++ lifetracker-api/lifetracker-schema.sql
@@ -0,0 +1,44 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY, 
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL, 
+    email TEXT NOT NULL UNIQUE CHECK (POSITION('@') IN email > 1),
+    password TEXT NOT NULL, 
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+)
+
+CREATE TABLE exercise (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    category TEXT NOT NULL,
+    duration INTEGER NOT NULL,
+    intensity INTEGER NOT NULL CHECK(intensity > 0 AND intensity < 11),
+    user_email TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    FOREIGN KEY (user_email) REFERENCES users(email) ON DELETE CASCADE
+)
+
+CREATE TABLE nutrition (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    category TEXT NOT NULL,
+    quantity INTEGER NOT NULL DEFAULT 0,
+    calories INTEGER NOT NULL DEFAULT 0,
+    image TEXT NOT NULL,
+    user_email TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    FOREIGN KEY (user_email) REFERENCES users(email) ON DELETE CASCADE
+)
+
+CREATE TABLE sleep (
+    id SERIAL PRIMARY KEY,
+    start_time TIMESTAMP NOT NULL,
+    end_time TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    user_id INTEGER NOT NULL,
+    user_email TEXT NOT NULL,
+    FOREIGN KEY (user_email) REFERENCES users(email) ON DELETE CASCADE
+)

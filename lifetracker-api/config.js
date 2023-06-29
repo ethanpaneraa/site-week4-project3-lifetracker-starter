ad30df52cd7fa7344ed4c434f2c5c17dfc973b0e
Ethan Pineda <joshuapineda66@gmail.com>
2023-06-28 23:01:33 -0700

    config file done

---
+++ lifetracker-api/config.js
@@ -0,0 +1,32 @@
+// Desc: config file for lifetracker-api
+// define config for lifetracker-api
+require("dotenv").config();
+require("colors"); 
+
+// select default port in .env or use 3001 (development port)
+const PORT = process.env.PORT ? Number(process.env.port) : 3001; 
+
+// get secret key in .env
+const SECRET_KEY = process.env.SECRET_KEY || "CHANGE_THIS";
+
+// set work factor for hashing user passwords in database
+const BCRYPT_WORK_FACTOR = 10;
+
+// get database uri from .env
+function getDatabaseUri() {
+
+    const databaseUser = process.env.DATABASE_USER || "postgres"
+    const databasePassword = process.env.DATABASE_PASS ? encodeURI(process.env.DATABASE_PASS) : "postgres";
+    const databaseHost = process.env.DATABASE_HOST || "localhost";
+    const databasePort = process.env.DATABASE_PORT || 5433;
+    const databaseName = process.env.DATABASE_NAME || "lifetracker";
+
+    return process.env.DATABASE_URL || `postgresql://${databaseUser}:${databasePassword}@${databaseHost}:${databasePort}/${databaseName}`;
+}
+
+module.exports = {
+    PORT, 
+    SECRET_KEY,
+    BCRYPT_WORK_FACTOR,
+    getDatabaseUri
+}
